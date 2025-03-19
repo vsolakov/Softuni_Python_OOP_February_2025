70647cb2c60c13804d789dd0c8bdead49e677038
vsolakov <veselinsolakov@gmail.com>
2025-03-19 23:10:15 +0200

    Upload finished tasks

---
+++ 05_Static_and_Class_Methods_Exercise/02_movie_world/project/movie_world.py
@@ -0,0 +1,54 @@
+from project.customer import Customer
+from project.dvd import DVD
+from typing import List
+class MovieWorld:
+    def __init__(self, name: str):
+        self.name = name
+        self.customers: List[Customer] = []
+        self.dvds: List[DVD] = []
+
+    @staticmethod
+    def dvd_capacity():
+        return 15
+
+    @staticmethod
+    def customer_capacity():
+        return 10
+
+    def add_customer(self, customer: Customer):
+        if len(self.customers) < 10:
+            self.customers.append(customer)
+
+    def add_dvd(self, dvd: DVD):
+        if len(self.dvds) < 15:
+            self.dvds.append(dvd)
+
+    def rent_dvd(self, customer_id: int, dvd_id: int):
+        customer = next((c for c in self.customers if c.id == customer_id), None)
+        dvd = next((d for d in self.dvds if d.id == dvd_id), None)
+        if dvd in customer.rented_dvds:
+            return f"{customer.name} has already rented {dvd.name}"
+        for c in self.customers:
+            if c.id != customer_id:
+                if dvd in c.rented_dvds:
+                    return "DVD is already rented"
+        if customer.age < dvd.age_restriction:
+            return f"{customer.name} should be at least {dvd.age_restriction} to rent this movie"
+        customer.rented_dvds.append(dvd)
+        dvd.is_rented = True
+        return f"{customer.name} has successfully rented {dvd.name}"
+
+    def return_dvd(self, customer_id, dvd_id):
+        customer = next((c for c in self.customers if c.id == customer_id), None)
+        dvd = next((d for d in self.dvds if d.id == dvd_id), None)
+        if dvd in customer.rented_dvds:
+            customer.rented_dvds.remove(dvd)
+            dvd.is_rented = False
+            return f"{customer.name} has successfully returned {dvd.name}"
+        return f"{customer.name} does not have that DVD"
+
+    def __repr__(self):
+        result = [c.__repr__() for c in self.customers]
+        result.extend([d.__repr__() for d in self.dvds])
+        return '\n'.join(result)
+
